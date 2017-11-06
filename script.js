3f04627d26c03b9cab8a7d7e54bfe0dd607f8697
Harshit Luthra <love.coolsachin@gmail.com>
2017-11-06 14:18:03 +0530

    fixed everything Noweverything is working

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ $(document).ready(function() {
   console.log('All assets are loaded');
     $('#load-screen').addClass('loaded');
 });
-},4000);
+     $('.element').typed({
+   strings: ['<i>Harshit Luthra</i>','<strong>Mohit Sharma</strong>', 'Harshita Aggu'],
+    typeSpeed: 0,
+    backSpeed: 40,
+         startDelay:2000,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+    
+},3000);
+
     
